1342a4f2459f028ebddf270d2c917f21c765302b
devon96 <kdryja@gmail.com>
2020-02-05 13:13:09 +0000

    Add func to combine fragmented packets.

--- code/flytrap/proxy.go
+++ code/flytrap/proxy.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/hex"
 	"io"
 	"io/ioutil"
 	"log"
@@ -17,6 +18,22 @@ type proxy struct {
 	lc, rc net.Conn
 }
 
+func readFull(r io.ReadCloser) ([]byte, error) {
+	// First two bytes of the packet represent Control type and remaining length - needed to read the entire packet.
+	h := make([]byte, 2)
+	_, err := io.ReadFull(r, h)
+	if err != nil {
+		return nil, err
+	}
+	// Second byte indicates remaining length of the packet.
+	v := make([]byte, h[1])
+	_, err = io.ReadFull(r, v)
+	if err != nil {
+		return nil, err
+	}
+	return append(h, v...), nil
+}
+
 func proxyPass(ctx context.Context, lc, rc net.Conn, p *proxy) error {
 	for {
 		select {
@@ -24,17 +41,26 @@ func proxyPass(ctx context.Context, lc, rc net.Conn, p *proxy) error {
 			return nil
 		default:
 		}
-		b := make([]byte, 4096)
-		n, err := lc.Read(b)
+		data, err := readFull(lc)
 		if err != nil {
 			return err
 		}
-		data := make([]byte, n)
-		copy(data, b[:n])
-		if p.lc.RemoteAddr() == lc.RemoteAddr() {
-			//TODO(kdryja): Process data - which is TCP packet
-			log.Print("Left side of the proxy")
-		}
+		log.Println(hex.Dump(data))
+		// Check if it's CONNECT packet
+		// if data[0] == 0x10 {
+		// 	// Identify the position of two bytes indicating length of ClientID
+		// 	// This is important, as the Protocal Name length varies - which is stored in 3rd and 4th byte.
+		// 	i := 8 + (data[2]<<2 | data[3])
+		// 	// Identify length of ClientID, which is the combination of two bytes
+		// 	idLen := data[i]<<2 | data[i+1]
+		// 	i += 2
+		// 	// Strip username and password from the payload.
+		// 	data = data[:i+idLen]
+		// 	// Change the total-length byte with respect to the removed username/password.
+		// 	data[1] = byte(len(data) - 2)
+		// 	// Clear Password and Username Connect Flag.
+		// 	data[i-5] = data[i-5] & 0x3F
+		// }
 		_, err = rc.Write(data)
 		if err != nil {
 			return err
